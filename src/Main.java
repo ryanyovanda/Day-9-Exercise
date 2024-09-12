58d0e0499e33213066911550f6de7552e99496e7
Ryan Yovanda <ryanyovanda@gmail.com>
2024-09-12 14:22:29 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+class Login {
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String toString() {
+        return username + " " + password;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        private LoginMenu<LoginMenuList> login;
+
+    }
+    public main() {
+
+
+    }
+}
